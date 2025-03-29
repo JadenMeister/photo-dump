306f2890f77352c779c1efb127983648a168370a
최정민 <bigdragon00@naver.com>
2025-03-29 18:00:42 +0900

    버튼 상태관리, 배경 수정

--- src/App.js
+++ src/App.js
@@ -27,7 +27,6 @@ function App() {
           element={
             <div className="app">
               <SpaceBackground/>
-
               {showLogin && <LoginModal />}
             </div>
           }
