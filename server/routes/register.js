619295a36ab22d8312cf2d48074822e592d9778d
최정민 <bigdragon00@naver.com>
2025-03-31 19:26:10 +0900

    세션 회원가입, 빈 데이터 넘김 방지 추가

--- server/routes/register.js
+++ server/routes/register.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const bcrypt = require("bcryptjs");
 const session = require("express-session");
 const pool = require("../config/database");
-router.post("/api/register", async (req, res) => {
+router.post("/", async (req, res) => {
     try {
         const { username, password } = req.body;
 
@@ -18,13 +18,23 @@ router.post("/api/register", async (req, res) => {
             return res.status(400).json({ msg: "이미 존재하는 사용자입니다." });
         }
 
+        if (!username || !password) {
+            return res.status(400).json({msg:"올바른 값을 입력해주세요"});
+        }
+
         const salt = await bcrypt.genSalt(10);
         const hashedPassword = await bcrypt.hash(password, salt);
 
         const [result] = await pool.execute(
             "INSERT INTO users (username, password) VALUES (?, ?)",
             [username, hashedPassword]
+
         );
+        return res.status(200).json({
+            msg: "회원가입 성공",
+            username
+        });
+
 
         const payload = {
             user: {
