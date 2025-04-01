1791197c4ab6c506354fbfbe9fa17dec7744a0fd
최정민 <bigdragon00@naver.com>
2025-04-01 19:35:10 +0900

    dev: login close button function

--- src/components/mapUtil/UploadTooltip.jsx
+++ src/components/mapUtil/UploadTooltip.jsx
@@ -5,13 +5,19 @@ import {Tooltip} from "react-tooltip";
 
 export function UploadTooltip({selectedContinent, setSelectedContinent }) {
 
+    const backgroundClick = (e)=>{
+        if(e.target.classList.contains("upload-modal")){
+            setSelectedContinent(null);
+
+        }
+
+    }
+
     return (
-
-        <div className="upload-modal">
-
-
+        <div className="upload-modal" onClick={backgroundClick}>
 
             <div className="close" onClick={() => setSelectedContinent(null)}>x</div>
+
             <h3>{selectedContinent} 사진 업로드</h3>
             <input type="file" accept="image/*"/>
             <button>올리기</button>
