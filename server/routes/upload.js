dde6c04fc571f87c61d36b89c1a377258f78df92
최정민 <bigdragon00@naver.com>
2025-04-05 22:15:06 +0900

    dev: upload 라우터 구현중, 이미지는 디비에 base64로 인코딩, 문자열로 디비에 저장 후 base64를 인코딩해서 이미지로 출력하는 방식 채탱

---
+++ server/routes/upload.js
@@ -0,0 +1,28 @@
+const router = require("express").Router();
+const pool = require("../config/database");
+const express = require("express");
+const multer = require("multer");
+
+
+
+
+router.get("/", async(req,res )=>{
+    try{
+        const {country_name, photoBase64} = req.query
+        const userId = req.session.user.id
+
+        await pool.execute(
+            "INSERT INTO country (country_name, user_id, photoBase64) VALUES (?, ?, ?)",
+            [country_name, userId, photoBase64]
+        );
+        res.status(200).json({msg: "업로드 성공"})
+
+
+
+
+
+    }catch(error){
+        console.error("에러", error)
+        res.status(500).json({msg: "서버 오류"})
+    }
+})
