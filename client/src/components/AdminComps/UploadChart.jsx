c537c660e0adba8e4a66b06077cfadc60908c58c
최정민 <bigdragon00@naver.com>
2025-04-25 23:05:30 +0900

    어드민 페이지 업로드 관련 컴포넌트들

---
+++ client/src/components/AdminComps/UploadChart.jsx
@@ -0,0 +1,13 @@
+
+
+export default function UploadChart() {
+    return(
+
+        <div className="w-108 h-170.25 flex items-center justify-center bg-[#F5F5F5]  shadow-lg">
+            나라별 업로드통계
+        </div>
+
+
+
+    )
+}
---
+++ client/src/components/AdminComps/UploadStatus.jsx
@@ -0,0 +1,11 @@
+
+export default function UploadStatus({ status }) {
+  return (
+      <div className="w-108.5 h-30 flex items-center justify-center bg-[#F5F5F5]  shadow-lg">
+
+          업로드 통계
+
+      </div>
+  );
+}
+
