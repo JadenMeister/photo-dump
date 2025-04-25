37d4cc29fb73ce43fbc229bdfccec1658a8a4a21
최정민 <bigdragon00@naver.com>
2025-04-25 23:06:05 +0900

    어드민 페이지 유저관련 컴포넌트

---
+++ client/src/components/AdminComps/UserManage.jsx
@@ -0,0 +1,9 @@
+
+
+export default function UserManage() {
+    return(
+        <div className="w-245 h-170.25 flex items-center justify-center bg-[#F5F5F5]  shadow-lg">
+            유저 관리
+        </div>
+    )
+}
