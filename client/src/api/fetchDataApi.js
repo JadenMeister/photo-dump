655948f496f47f8f0cf276e7fad019166b0b5f4f
최정민 <bigdragon00@naver.com>
2025-04-26 21:37:47 +0900

    dev: 데이터 호출 전역함수로 관리

--- client/src/api/fetchDataApi.js
+++ client/src/api/fetchDataApi.js
@@ -1,13 +1,11 @@
-export function uploadImage(selectedCountry, imageFile) {
-    const formData = new FormData();
-    formData.append("country", selectedCountry);
-    formData.append("image", imageFile);}
+export const fetchCountries = async () => {
+    const response = await fetch("http://localhost:8080/admin/uploads");
+    if (!response.ok) throw new Error("국가 데이터 가져오기 실패");
+    return await response.json();
+};
 
-
-await fetch("/api/upload",{
-    method: "POST",
-    headers: {
-        "Content-Type": "application/json",
-    },
-    body: JSON.stringify(formData),
-})
+export const fetchUsers = async () => {
+    const response = await fetch("http://localhost:8080/admin/users");
+    if (!response.ok) throw new Error("유저 데이터 가져오기 실패");
+    return await response.json();
+};
