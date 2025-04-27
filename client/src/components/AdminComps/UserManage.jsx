88f9faed33b2ca5da119b97b04f5e08297a3f3ad
최정민 <bigdragon00@naver.com>
2025-04-27 23:07:18 +0900

    dev: react-icon 설치, 유저관리 표 구성 및 표 내 meatball menu icon 추가

--- client/src/components/AdminComps/UserManage.jsx
+++ client/src/components/AdminComps/UserManage.jsx
@@ -1,11 +1,18 @@
 import {useState,useEffect} from "react";
 import {useAuth} from "../../context/AuthContext";
 import {fetchUsers} from "../../api/fetchDataApi.js";
+import { HiDotsHorizontal } from "react-icons/hi";
+
 
 export default function UserManage() {
     const {user} = useAuth();
     const [userData, setUserData] = useState([]);
     const [isLoading, setIsLoading] = useState(true);
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setIsOpen(prev => !prev);
+    }
 
 
     useEffect(() => {
@@ -35,27 +42,48 @@ export default function UserManage() {
     const tableHeader = ["아이디", "이메일", "권한", "가입날짜", "관리"]
 
     return(
-        <div className="w-245 h-170.25 flex items-center justify-center bg-[#F5F5F5]  shadow-lg">
-            <table className="w-full h-full">
-                <thead>
-                    <tr>
-                        {tableHeader.map((header, index) => (
-                            <th key={index} className="border-b px-4 text-center">
-                                {header}
-                            </th>
-                        ))}
-                    </tr>
+
+
+
+        <div className="w-245 h-170.25 flex flex-col justify-start  bg-[#F5F5F5]  shadow-lg">
+            <div className="mb-5 mt-5 text-center text-2xl font-bold text-gray-700">
+                회원관리
+            </div>
+            <table className="w-full  overflow-scroll">
+
+                <thead className="bg-gray-300 ">
+                <tr>
+                    {tableHeader.map((header, index) => (
+                        <th key={index} className="border-b px-4 py-2 text-center">
+                            {header}
+                        </th>
+                    ))}
+                </tr>
                 </thead>
                 <tbody>
                     {userData.map((user) => (
                         <tr key={user.id} className="overflow-auto">
-                            <td className="border-b text-center">{user.username}</td>
-                            <td className="border-b text-center">{user.email}</td>
-                            <td className="border-b text-center">{user.role}</td>
-                            <td className="border-b text-center">{new Date(user.created_at).toLocaleDateString()}</td>
+                            <td className="border-b py-2 px-2 text-center ">{user.username}</td>
+                            <td className="border-b py-2 px-2 text-center">{user.email}</td>
+                            <td className="border-b py-2 px-2 text-center">{user.role}</td>
+                            <td className="border-b py-2 px-2 text-center">{new Date(user.created_at).toLocaleDateString()}</td>
                             <td className="border-b px-4 text-center">
-                                <button className="bg-red-500 text-white px-2 py-1 rounded">삭제</button>
+                                <button onClick={toggleMenu} className="text-black px-2 py-1rounded">
+
+                                    <HiDotsHorizontal className="text-sm cursor-pointer"/>
+                                </button>
                             </td>
+                            {isOpen &&(
+                                <div className="absolute bg-white shadow-lg rounded-md mt-2">
+                                    <ul className="py-2">
+                                        <li className="px-4 py-2 hover:bg-gray-200 cursor-pointer">수정</li>
+                                        <li className="px-4 py-2 hover:bg-gray-200 cursor-pointer">삭제</li>
+                                        <li className="px-4 py-2 hover:bg-gray-200 cursor-pointer">사진 보기</li>
+                                    </ul>
+                                </div>
+                            )}
+
+
 
                         </tr>
                     ))}
