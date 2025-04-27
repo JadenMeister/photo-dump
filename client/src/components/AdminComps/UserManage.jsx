cc614fa8d09c12981c7080b4d22efb8dfe9b68e3
최정민 <bigdragon00@naver.com>
2025-04-27 18:18:08 +0900

    dev:어드민페이지에서 데이터 못 불러오는 문제 수정 중

--- client/src/components/AdminComps/UserManage.jsx
+++ client/src/components/AdminComps/UserManage.jsx
@@ -1,9 +1,59 @@
-
+import {useState,useEffect} from "react";
+import {useAuth} from "../../context/AuthContext";
+import {fetchUsers} from "../../api/fetchDataApi.js";
 
 export default function UserManage() {
+    const {user} = useAuth();
+    const [userData, setUserData] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
+
+
+    useEffect(() => {
+        const getUserData = async () => {
+            try{
+                const data = await fetchUsers();
+                setUserData(data);
+            } catch(err){
+                console.error("유저 데이터 가져오기 실패", err);
+            } finally {
+                setIsLoading(false);
+            }
+        }
+        getUserData()
+    }, []);
+
+    if(isLoading){
+        return(
+            <div className="w-245 h-170.25 flex items-center justify-center bg-[#F5F5F5]  shadow-lg">
+                <p>Loading...</p>
+            </div>
+        )
+    }
+
+    const th = ["아이디", "이메일", "권한", "관리"]
+
     return(
         <div className="w-245 h-170.25 flex items-center justify-center bg-[#F5F5F5]  shadow-lg">
-            유저 관리
+            <table className="w-full h-full">
+                <thead>
+                    <tr>
+                        {th.map((header, index) => (
+                            <th key={index} className="border-b px-4 py-2 text-center">
+                                {header}
+                            </th>
+                        ))}
+                    </tr>
+                </thead>
+                <tbody>
+                    {userData.map((user) => (
+                        <tr key={user.id}>
+                            <td className="border px-4 py-2">{user.username}</td>
+                            <td className="border px-4 py-2">{user.email}</td>
+                            <td className="border px-4 py-2">{user.role}</td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
         </div>
     )
 }
