f6d96f87098d3de34157c8aaeae79c646bc8dc5d
최정민 <bigdragon00@naver.com>
2025-04-27 19:06:24 +0900

    dev: 데이터 호출 성동

--- client/src/components/AdminComps/UserManage.jsx
+++ client/src/components/AdminComps/UserManage.jsx
@@ -13,6 +13,8 @@ export default function UserManage() {
             try{
                 const data = await fetchUsers();
                 setUserData(data);
+                console.log("유저 데이터", data); //디버깅용
+                console.log("유저 데이터", userData); //디버깅용
             } catch(err){
                 console.error("유저 데이터 가져오기 실패", err);
             } finally {
@@ -30,15 +32,15 @@ export default function UserManage() {
         )
     }
 
-    const th = ["아이디", "이메일", "권한", "관리"]
+    const tableHeader = ["아이디", "이메일", "권한", "가입날짜", "관리"]
 
     return(
         <div className="w-245 h-170.25 flex items-center justify-center bg-[#F5F5F5]  shadow-lg">
             <table className="w-full h-full">
                 <thead>
                     <tr>
-                        {th.map((header, index) => (
-                            <th key={index} className="border-b px-4 py-2 text-center">
+                        {tableHeader.map((header, index) => (
+                            <th key={index} className="border-b px-4 text-center">
                                 {header}
                             </th>
                         ))}
@@ -46,10 +48,15 @@ export default function UserManage() {
                 </thead>
                 <tbody>
                     {userData.map((user) => (
-                        <tr key={user.id}>
-                            <td className="border px-4 py-2">{user.username}</td>
-                            <td className="border px-4 py-2">{user.email}</td>
-                            <td className="border px-4 py-2">{user.role}</td>
+                        <tr key={user.id} className="overflow-auto">
+                            <td className="border-b text-center">{user.username}</td>
+                            <td className="border-b text-center">{user.email}</td>
+                            <td className="border-b text-center">{user.role}</td>
+                            <td className="border-b text-center">{new Date(user.created_at).toLocaleDateString()}</td>
+                            <td className="border-b px-4 text-center">
+                                <button className="bg-red-500 text-white px-2 py-1 rounded">삭제</button>
+                            </td>
+
                         </tr>
                     ))}
                 </tbody>
