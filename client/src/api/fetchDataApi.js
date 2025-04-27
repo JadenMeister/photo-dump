c2abdf48d4deff3c46eab13dc033f722efb1baaa
최정민 <bigdragon00@naver.com>
2025-04-27 19:04:47 +0900

    dev: 호출 엔드포인트 수정

--- client/src/api/fetchDataApi.js
+++ client/src/api/fetchDataApi.js
@@ -1,11 +1,24 @@
 export const fetchCountries = async () => {
-    const response = await fetch("http://localhost:8080/admin/uploads");
+    const response = await fetch("http://localhost:8080/api/admin/uploads",{
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        credentials: "include",
+    });
     if (!response.ok) throw new Error("국가 데이터 가져오기 실패");
+
     return await response.json();
 };
 
 export const fetchUsers = async () => {
-    const response = await fetch("http://localhost:8080/admin/users");
+    const response = await fetch("http://localhost:8080/api/admin/users",{
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        credentials: "include",
+    });
     if (!response.ok) throw new Error("유저 데이터 가져오기 실패");
     return await response.json();
 };
