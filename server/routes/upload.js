7945024fe359f71cdbbaf0c21be44774884bbcfe
Jadenmeister <jadenmeister@Jadenmeisterui-Macmini.local>
2025-05-05 00:49:12 +0900

    dev: AWS v3로 변경, 미들웨어 위치 복구

--- server/routes/upload.js
+++ server/routes/upload.js
@@ -4,25 +4,25 @@ const express = require("express");
 const multer = require("multer");
 const AWS = require("aws-sdk");
 const multerS3 = require("multer-s3");
+const {S3Client} = require("@aws-sdk/client-s3");
 require("dotenv").config();
 
-// memeoryStorage를 사용하여 메모리에 파일 저장시에는 옵션 없이
-const multerStorage = multer.memoryStorage();
 
-//multer 설정
-const upload = multer({multerStorage})
 
-// AWS S3 설정
-const s3 = new AWS.S3({
-    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+// AWS S3 설정 v3로 변경
+const s3 = new S3Client({
     region: process.env.AWS_REGION,
+    credentials: {
+        accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
+    }
 });
 
+//multer 설정
 const upload = multer ({
     storage: multerS3({
         s3: s3,
-        bucket: process.env.AWS_BUCKET_NAME,
+        bucket: process.env.AWS_S3_BUCKET,
         acl: "public-read",
         contentType: multerS3.AUTO_CONTENT_TYPE,
         key: function (req, file, cb) {
@@ -35,23 +35,24 @@ const upload = multer ({
 
 
 
-router.post("/", upload.single("file"), async (req,res )=>{
+router.post("/", upload.single("photo"), async (req,res )=>{
     try{
-        const {user_id, country, travel_date} = req.body;
-        const photoUrl = req.file.location;
-
-        await req.db.execute("INSERT INTO photos (user_id, country, travel_date, photo_url) VALUES (?, ?, ?, ?)",
-
-            [user_id, country, travel_date, photoUrl]);
-
-
         if(!req.file){
             return res.status(400).json({msg: "업로드 할 사진이 없습니다."});
-        } else {
-            const base64 = req.file.buffer.toString("base64");
-            res.status(200).json({msg:"업로드 성공"});
         }
-        
+
+        const {user_id, country_id, travel_date} = req.body;
+
+        const photoUrl = req.file.location;
+
+        await req.db.execute("INSERT INTO photos (user_id, country_id, travel_date, photo_url) VALUES (?, ?, ?, ?)",
+
+            [user_id, country_id, travel_date,  photoUrl]
+        );
+
+        res.status(200).json({ msg: "업로드 성공", photo_url: photoUrl });
+
+
     } catch (err){
         console.error("파일 업로드 실패", err);
         res.status(500).json({ msg: "파일 업로드 실패" });
