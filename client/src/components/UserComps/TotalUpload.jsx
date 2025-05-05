7ac721304927054699238bbe1ec707aa44552672
Jadenmeister <bigdragon00@naver.com>
2025-05-05 23:05:02 +0900

    dev: 유저의 총 업로드 수 가져오기 완료

--- client/src/components/UserComps/TotalUpload.jsx
+++ client/src/components/UserComps/TotalUpload.jsx
@@ -3,14 +3,14 @@ import {fetchUserUploadCount} from "@/api/fetchDataApi.js";
 
 export default function TotalUpload() {
 
-  const [count, setCount] = useState(null);
+  const [count, setCount] = useState(0);
 
 
   useEffect(() => {
     const fetchCount = async () => {
       try {
         const response = await fetchUserUploadCount();
-        setCount(response.data);
+        setCount(response.upload_count);
       } catch (err) {
         console.error("업로드 수 가져오기 실패", err);
       }
@@ -20,5 +20,16 @@ export default function TotalUpload() {
 
 
 
-  }, []);
+  }, [])
+
+return (
+    <div className="w-26% h-1/2 bg-[#F5F5F5] rounded-lg shadow-lg flex flex-col items-center justify-center">
+      <h2 className="text-2xl font-bold">업로드 수</h2>
+      <p className="text-2xl font-bold">{count}</p>
+    </div>
+
+)
+
+
 }
+
