a78ad6263fac38dc60de854b4d2f42e8292229af
Jadenmeister <bigdragon00@naver.com>
2025-05-11 18:22:18 +0900

    feat: 세션 스토리지 저장 관련 수정

--- client/src/context/AuthContext.jsx
+++ client/src/context/AuthContext.jsx
@@ -1,6 +1,7 @@
 import { useState, useContext, createContext, useEffect } from "react";
 
 
+
 const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
@@ -8,50 +9,27 @@ export const AuthProvider = ({ children }) => {
     const [user, setUser] = useState(null);
 
 
-    const loginData = (userData) => {
-        setUser({
-            id: userData.id,
-            username: userData.username,
-            role: userData.role,
-            permissions: userData.permissions || []
-        });
-        sessionStorage.setItem("id", userData.id);
-        sessionStorage.setItem("username", userData.username);
-        sessionStorage.setItem("role", userData.role);
-        sessionStorage.setItem("permissions", JSON.stringify(userData.permissions || []));
+
+    useEffect(() => {
+        const stored = sessionStorage.getItem("user");
+        console.log("session user:", stored);
+        if (stored) {
+            const sessionData = JSON.parse(stored);
+            setUser(sessionData);
+            setIsLogin(true);
+        }
+    }, []);
+
+    const handleLogin = (userData) => {
+        setUser(userData);
         setIsLogin(true);
-    };
+        sessionStorage.setItem("user", JSON.stringify(userData));
 
-
-
-    useEffect(()=>{
-        fetch('http://localhost:8080/api/authCheck/',{
-
-            method: 'GET',
-            credentials: 'include',
-        })
-            .then(res => {
-                if(res.ok){
-                    return res.json();
-                } else{
-                    throw new Error("로그인 필요");
-                }
-            })
-            .then((data =>{
-                if(data?.user){
-                    setIsLogin(true);
-                    setUser(data.user);
-                }
-            })
-            )
-
-    },[])
-
-    const handleLogin = () => {
-        setIsLogin(true);
     }
 
     const handleLogout = () => {
+
+        setUser(null);
         setIsLogin(false);
         sessionStorage.clear();
     }
@@ -60,7 +38,7 @@ export const AuthProvider = ({ children }) => {
 
 
     return (
-        <AuthContext.Provider value={{ isLogin, setIsLogin, handleLogin, handleLogout, loginData, user, setUser }}>
+        <AuthContext.Provider value={{ isLogin, setIsLogin, handleLogin, handleLogout, user, setUser }}>
             {children}
         </AuthContext.Provider>
     );
