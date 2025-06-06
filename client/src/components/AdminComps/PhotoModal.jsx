541f460e6904dc60548357459c6912f359ff83ad
Jadenmeister <bigdragon00@naver.com>
2025-06-07 01:25:54 +0900

    fix: 사진 자세히 보기 모달 수정

--- client/src/components/AdminComps/PhotoModal.jsx
+++ client/src/components/AdminComps/PhotoModal.jsx
@@ -1,28 +1,44 @@
-import { Dialog, Portal, Stack, CloseButton } from "@chakra-ui/react";
+import {Dialog, Portal, Stack, CloseButton, GridItem, Grid} from "@chakra-ui/react";
+
 
 export default function PhotoModal({ isOpen, onClose, photos }) {
-  console.log("모달 뜸 ") // 디버깅용
+
+
+
+
+  const photoCountry = photos.reduce((acc,photo)=>{
+    if (!acc[photo.country_name]) {
+      acc[photo.country_name] = [];
+    }
+    acc[photo.country_name].push(photo);
+    return acc;
+  }, {});
+
+
+
   return (
       <Dialog.Root open={isOpen} onChange={(open)=> !open && onClose()}>
         <Portal>
-          <Dialog.Backdrop />
+          <Dialog.Backdrop onClick={e=>{if(e.target === e.currentTarget)onClose()}} />
           <Dialog.Positioner>
-            <Dialog.Content className=" w-[800px] bg-white rounded-lg shadow-lg p-5 flex ">
-              <Stack spacing={4}>
-                {photos.map((photo) => (
-                    <div key={photo.id} className="flex flex-col items-center">
-                      <img
-                          src={photo.photo_url}
-                          alt={photo.country_name}
-                          className="w-full h-auto rounded-lg mb-2"
-                      />
-                      <h3 className="text-lg font-semibold">{photo.country_name}</h3>
-                      <p className="text-sm text-gray-500">
-                        {new Date(photo.travel_date).toLocaleDateString()}
-                      </p>
-                    </div>
-                ))}
-              </Stack>
+            <Dialog.Content className=" w-[800px] bg-white rounded-lg shadow-lg p-5 ">
+              {Object.entries(photoCountry).map(([country, countryPhotos]) => (
+                  <div key={country} className="mb-8">
+                    <h3 className="text-xl font-bold mb-2 border-b">{country}</h3>
+                    <Grid templateColumns="repeat(3, 1fr)" gap="6">
+                      {countryPhotos.map((photo) => (
+                          <GridItem key={photo.id} className="flex flex-col items-center">
+                            <img
+                                src={photo.photo_url}
+                                alt={photo.country_name}
+                                className="w-full h-auto rounded-lg mb-2"
+                                style={{ height: '200px', maxHeight: '250px', objectFit: 'cover' }}
+                            />
+                          </GridItem>
+                      ))}
+                    </Grid>
+                  </div>
+              ))}
               <Dialog.CloseTrigger asChild>
               <CloseButton size="lg" color="white" onClick={onClose}/>
               </Dialog.CloseTrigger>
