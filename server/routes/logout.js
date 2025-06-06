6fbde4f0dfc3eeee5e5af2eef47d5e875ad5c1f1
Jadenmeister <bigdragon00@naver.com>
2025-06-06 14:53:41 +0900

    fix: 서버 로그아웃 수정

--- server/routes/logout.js
+++ server/routes/logout.js
@@ -5,7 +5,7 @@ const router = express.Router();
 
 
 router.get("/", (req, res) => {
-    res.session.destroy((err) => {
+    req.session.destroy((err) => {
         if(err){
             res.status(500).json({msg: "로그아웃실패"})
     } else{
